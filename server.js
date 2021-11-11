51cf5e40aaa3c9ebbf9d486af54dd85524991dd5
Jens Van de Voorde <jens.van.de.voorde@student.ehb.be>
2021-11-11 11:39:32 +0100

    feat(endpoints): created 3 new endpoints

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const server = express();
 const PORT = 3000;
+/**
+ * Makes server run on a port
+ */
+server.listen(PORT, () => {
+    console.log(`server is listening at port ${PORT}`);
+});
+const player = {
+    username: "johnnyboy"
+}
+/**
+ * gets info of a player
+ */
+server.get("/users", (req, res) => {
+    res.send(player);
+});
+/**
+ * posts a new player
+ */
+server.post("/users").send("username=johnnyboy");
+/**
+ * updates the highscore of a player
+ */
+server.update("/highscores").send("highscore=99");
